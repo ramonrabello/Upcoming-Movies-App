13625876c9d4f40e50e2793d207e2de07f1c936a
ramon.rabello <ramon.rabello@gmail.com>
2018-07-25 00:41:56 -0300

    updating TextViews typefaces.

--- app/src/main/java/com/arctouch/codechallenge/home/HomeAdapter.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeAdapter.kt
@@ -6,33 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.arctouch.codechallenge.R
+import com.arctouch.codechallenge.core.ktx.toTypeface
 import com.arctouch.codechallenge.details.MovieDetailsActivity
 import com.arctouch.codechallenge.model.Movie
 import com.arctouch.codechallenge.util.MovieImageUrlBuilder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.content_movie_details.movie_genres
+import kotlinx.android.synthetic.main.content_movie_details.movie_name
+import kotlinx.android.synthetic.main.content_movie_details.movie_release_date
 import kotlinx.android.synthetic.main.movie_item.view.*
 
-class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+/**
+ * Adapter that holds movie items.
+ */
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
     private val movies = mutableListOf<Movie>()
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(movie: Movie) {
-            itemView.titleTextView.text = movie.title
-            itemView.genresTextView.text = movie.genres?.joinToString(separator = ", ") { it.name }
-            itemView.releaseDateTextView.text = movie.releaseDate
+            itemView.apply {
+                titleTextView.text = movie.title
+                genresTextView.text = movie.genres?.joinToString(separator = ", ") { it.name }
+                releaseDateTextView.text = movie.releaseDate
+                Glide.with(itemView.context)
+                        .load(movie.posterPath?.let { MovieImageUrlBuilder.buildPosterUrl(it) })
+                        .apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                        .into(posterImageView)
 
-            Glide.with(itemView)
-                .load(movie.posterPath?.let { MovieImageUrlBuilder.buildPosterUrl(it) })
-                .apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
-                .into(itemView.posterImageView)
+                setOnClickListener {
+                    val context = itemView.context
+                    val intent = Intent(context, MovieDetailsActivity::class.java)
+                    intent.putExtra(MovieDetailsActivity.EXTRA_MOVIE, movie)
+                    context.startActivity(intent)
+                }
 
-            itemView.setOnClickListener {
-                val context = itemView.context
-                val intent = Intent(context, MovieDetailsActivity::class.java)
-                intent.putExtra(MovieDetailsActivity.EXTRA_MOVIE, movie)
-                context.startActivity(intent)
+                titleTextView.toTypeface("OpenSans-SemiBold")
+                releaseDateTextView.toTypeface("OpenSans-Regular")
+                genresTextView.toTypeface("OpenSans-Light")
             }
         }
     }
